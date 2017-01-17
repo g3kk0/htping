d2f81c18f18656d1a089645534dc5111335f247e
Mark Newman <mark@carrenza.com>
2017-01-17 11:55:06 +0000

    Added support for count and interval flags

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # HyperText Ping
 
-Inspired by ping, htping is a tool for testing/monitoring HTTP endpoints.
+Inspired by ping, htping is a tool for testing and monitoring HTTP endpoints.
 
 ```
 $ htping www.google.co.uk
-connected to 172.217.23.3:80, seq=0, time=0 ms, response=200
-connected to 172.217.23.3:80, seq=1, time=0 ms, response=200
-connected to 172.217.23.3:80, seq=2, time=0 ms, response=200
-connected to 172.217.23.3:80, seq=3, time=0 ms, response=200
+connected to 172.217.23.3:80, seq=0, time=68 ms, response=200
+connected to 172.217.23.3:80, seq=1, time=68 ms, response=200
+connected to 172.217.23.3:80, seq=2, time=67 ms, response=200
+connected to 172.217.23.3:80, seq=3, time=74 ms, response=200
 ```
 
 ## Installation
@@ -23,5 +23,5 @@ wget https://github.com/g3kk0/htping/releases/download/1.0.0/htping
 chmod 755 htping
 
 $ htping www.google.co.uk
-connected to 172.217.23.3:80, seq=0, time=0 ms, response=200
+connected to 172.217.23.3:80, seq=0, time=18 ms, response=200
 ```
--- main.go
+++ main.go
@@ -1,18 +1,68 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
-	"os"
 	"regexp"
 	"strconv"
 	"time"
 )
 
-func targetParser(s string) (string, string, string){
-	//protocol
+func main() {
+	count := flag.Int("c", 100, "Count")
+	interval := flag.Int("i", 1, "Interval")
+	flag.Parse()
+	target := flag.Arg(0)
+	protocol, domain, port := targetParser(target)
+	ip := ip(domain)
+
+	for i := 0; ; i++ {
+		if i >= *count {
+			break
+		}
+		response, elapsed := httpGet(protocol + "://" + domain + ":" + port)
+		fmt.Printf("connected to %s:%s, seq=%d, time=%d ms, response=%s\n", ip, port, i, elapsed, response)
+		time.Sleep(time.Duration(*interval) * time.Second)
+	}
+}
+
+func httpGet(s string) (string, int64) {
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+
+	start := time.Now()
+	resp, err := client.Get(s)
+	if err != nil {
+		return "\033[31mNo response\033[0m", 0
+	}
+	elapsed := int64(time.Since(start) / time.Millisecond)
+
+	statusCode := strconv.Itoa(resp.StatusCode)
+	if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
+		return "\033[32m" + statusCode + "\033[0m", elapsed
+	} else if resp.StatusCode >= 300 && resp.StatusCode <= 399 {
+		return "\033[33m" + statusCode + "\033[0m", elapsed
+	} else {
+		return "\033[31m" + statusCode + "\033[0m", elapsed
+	}
+}
+
+func ip(s string) string {
+	addr, err := net.LookupHost(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return addr[0]
+}
+
+func targetParser(s string) (string, string, string) {
+	// protocol
 	protoExpr, err := regexp.Compile("^https?")
 	if err != nil {
 		log.Fatal(err)
@@ -41,7 +91,7 @@ func targetParser(s string) (string, string, string){
 		port = "80"
 	}
 
-	//domain
+	// domain
 	domainExpr, err := regexp.Compile("^https?://")
 	if err != nil {
 		log.Fatal(err)
@@ -51,45 +101,4 @@ func targetParser(s string) (string, string, string){
 	domain = portExpr.ReplaceAllString(domain, "")
 
 	return protocol, domain, port
-}
-
-func ip(s string) string {
-	addr, err := net.LookupHost(s)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return addr[0]
-}
-
-func httpGet(s string) string {
-	client := &http.Client{
-		CheckRedirect: func(req *http.Request, via []*http.Request) error {
-			return http.ErrUseLastResponse
-		},
-	}
-
-	resp, err := client.Get(s)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	statusCode := strconv.Itoa(resp.StatusCode)
-
-	if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
-		return "\033[32m" + statusCode + "\033[0m"
-	} else if resp.StatusCode >= 300 && resp.StatusCode <= 399 {
-		return "\033[33m" + statusCode + "\033[0m"
-	} else {
-		return "\033[31m" + statusCode + "\033[0m"
-	}
-}
-
-func main() {
-	target := os.Args[1]
-	protocol, domain, port := targetParser(target)
-
-	for i := 0; ; i++ {
-		fmt.Printf("connected to %s:%s, seq=%d, time=0 ms, response=%s\n", ip(domain), port, i, httpGet(protocol+"://"+domain+":"+port))
-		time.Sleep(1000 * time.Millisecond)
-	}
 }
